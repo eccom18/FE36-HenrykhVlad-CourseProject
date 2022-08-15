d79298b0ac54d6d2ee9390562ee27241dbf91f19
eccom18 <eccom18@gmail.com>
2022-08-16 01:28:50 +0300

    added slider

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+new Swiper('.testimonial_slider', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+})
