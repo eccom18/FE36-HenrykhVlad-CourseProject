8e0ab3d993a9cfe12461aa6025a7f1691fb43429
eccom18 <eccom18@gmail.com>
2022-08-16 21:01:41 +0300

    fixed ht

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,15 @@ new Swiper('.testimonial_slider', {
     clickable: true,
   },
 })
+const smoothLinks = document.querySelectorAll('a[href^="#"]')
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener('click', function (e) {
+    e.preventDefault()
+    const id = smoothLink.getAttribute('href')
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  })
+}
